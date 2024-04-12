5b61022e3d709d0ae7370637b4ba8e4dedf1f32b
guilherme-lampert <guilhermenlampert@gmail.com>
2024-04-12 14:48:08 -0300

    no longer loading the db and config from the instance folder

--- recon/__init__.py
+++ recon/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app():
     """Application factory"""
 
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_pyfile('config.py')
 
 
