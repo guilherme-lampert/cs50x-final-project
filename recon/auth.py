2e5f5831ee344775099af7462fb962920fed877a
guilherme-lampert <guilhermenlampert@gmail.com>
2024-04-19 10:28:41 -0300

    Fixed routes in flask so the endpoints can work properly

--- recon/auth.py
+++ recon/auth.py
@@ -1,3 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template
 
-bp = Blueprint('auth', __name__)
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("auth/signup.html")
+
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("auth/login.html")
--- recon/query.py
+++ recon/query.py
@@ -1,10 +1,16 @@
 from flask import Blueprint, render_template
 from recon.db import get_db
 
-bp = Blueprint('query', __name__)
+bp = Blueprint("query", __name__)
 
 
-@bp.route('/')
+@bp.route("/")
 def index():
     """"""
     return render_template("query/index.html")
+
+
+@bp.route("/tutorial")
+def tutorial():
+    """"""
+    return render_template("query/tutorial.html")
