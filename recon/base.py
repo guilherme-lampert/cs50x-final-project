278c5b1dd46e46383c2953ec047204d171a12c73
guilherme-lampert <guilhermenlampert@gmail.com>
2024-05-13 14:47:47 -0300

    Project done

--- recon/base.py
+++ recon/base.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, current_app, request, jsonify, url_for
 from recon.db import get_db
+from recon.utils import load_categories, load_indicators
 
 bp = Blueprint("base", __name__)
 
@@ -14,5 +15,10 @@ def index():
 @bp.route("/tutorial")
 def tutorial():
     """Loads the tutorial page"""
+
+    db = get_db()
+
+    categories = load_categories()
+    indicators = load_indicators()
     
-    return render_template("base/tutorial.html")
+    return render_template("base/tutorial.html", indicators=indicators, categories=categories)
